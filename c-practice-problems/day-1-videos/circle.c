82a748734c6367462f4ed695240e6bdd0ae11d88
Michael <michaelbaltazar265@gmail.com>
2021-02-23 22:58:21 +0000

    day-1-finished

--- c-practice-problems/day-1-videos/circle.c
+++ c-practice-problems/day-1-videos/circle.c
@@ -5,7 +5,8 @@ int main ()
     int radius; // the distance from outside to center of the circle
     printf("please enter a radius: ");
     scanf("%i", &radius);  // address of operator
-
-    printf(" The given radius is: %i\n", radius ); 
+    double area = 3.14159 * (radius * radius); 
+    // ^ using double becasue its not a whole number.
+    printf("The area of a circle with %i radius is %f\n", radius , area); 
     return 0;
 }
